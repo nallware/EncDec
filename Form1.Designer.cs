345fce7d803e41e8f7ddce158e9f5502453a733b
nallware <dev@nallware.com>
2016-05-24 11:58:37 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace EncDec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDecryptedText = new System.Windows.Forms.TextBox();
+            this.tbEncryptedText = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSalt = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbDecryptedText
+            // 
+            this.tbDecryptedText.Location = new System.Drawing.Point(27, 32);
+            this.tbDecryptedText.Name = "tbDecryptedText";
+            this.tbDecryptedText.Size = new System.Drawing.Size(409, 20);
+            this.tbDecryptedText.TabIndex = 0;
+            // 
+            // tbEncryptedText
+            // 
+            this.tbEncryptedText.Location = new System.Drawing.Point(28, 81);
+            this.tbEncryptedText.Name = "tbEncryptedText";
+            this.tbEncryptedText.Size = new System.Drawing.Size(406, 20);
+            this.tbEncryptedText.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(199, 143);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(289, 145);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Decrypted";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(25, 65);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(55, 13);
+            this.lbl1.TabIndex = 5;
+            this.lbl1.Text = "Encrypted";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Salt";
+            // 
+            // tbSalt
+            // 
+            this.tbSalt.Location = new System.Drawing.Point(28, 145);
+            this.tbSalt.Name = "tbSalt";
+            this.tbSalt.Size = new System.Drawing.Size(100, 20);
+            this.tbSalt.TabIndex = 7;
+            this.tbSalt.Text = "generic";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(375, 145);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 210);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.tbSalt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.tbEncryptedText);
+            this.Controls.Add(this.tbDecryptedText);
+            this.Name = "Form1";
+            this.Text = "Encrypt/Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDecryptedText;
+        private System.Windows.Forms.TextBox tbEncryptedText;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSalt;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EncDec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                      
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string text = tbDecryptedText.Text;
+            string salt = tbSalt.Text;
+            tbEncryptedText.Text = babelFish.EncryptText(text, salt);
+            }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string salt = tbSalt.Text;
+            string text = tbEncryptedText.Text;
+            tbDecryptedText.Text = babelFish.DecryptText(text, salt);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbDecryptedText.Clear();
+            tbEncryptedText.Clear();
+            tbSalt.Clear();
+        }
+        
+    }
+}
